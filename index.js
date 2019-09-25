e1c51a0dc3917e46df4c5f626642494e6860207e
Rory Murray <rory.christian.murray@gmail.com>
2019-09-25 07:48:00 -0400

    Set-up server file with middleware and routes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // code away!
+const express = require("express");
+const server = require("./server");
 
-// const server = require("./server")
-
-// const port = 8000;
-// server.listen(port, () => {
-//   console.log(`Server running on port ${port}`)
-// })
+const port = 8000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express = require('express');
+// dependencies
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
+// initiate server instance
 const server = express();
 
 // import routes
-const postRoutes = require('./posts/postRouter')
+const postRoutes = require("./posts/postRouter");
+const userRoutes = require("./users/userRouter");
 
 // declare routes
 
 server.use("/posts", postRoutes);
+server.use("/users", userRoutes);
 
 // middleware
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
-});
+// server.get("/", (req, res) => {
+//   res.send(`<h2>Let's write some middleware!</h2>`);
+// });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
-
+// function logger(req, res, next) {}
 
 module.exports = server;
