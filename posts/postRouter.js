2ab9656d06aaa55700bfd7ed79805aea1ad70072
unknown <rory.christian.murray@gmail.com>
2019-09-26 08:47:48 -0400

    Create get posts and get post by id endpoint

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -4,10 +4,25 @@ const router = require("express").Router();
 const postDb = require("./postDb");
 
 router.get("/", (req, res) => {
-  res.send("you found me");
+  postDb.get()
+  .then(posts => {
+    res.status(200).json(posts);
+  })
+  .catch(() => {
+    res.status(500).json({ message: "Error retrieving posts"})
+  })
 });
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  postDb.getById(id)
+  .then(post => {
+    res.status(200).json(post);
+  })
+  .catch(() => {
+    res.status(500).json({message: "Error retrieving post by id"})
+  })
+});
 
 router.delete("/:id", (req, res) => {});
 
