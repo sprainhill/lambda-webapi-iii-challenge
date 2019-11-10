f703981ed549cf06e7da009f84fa4df4b2c1d242
Rory Murray <rory.christian.murray@gmail.com>
2019-11-10 17:53:05 -0500

    Add local middleware

--- server.js
+++ server.js
@@ -10,22 +10,50 @@ const server = express();
 const postRoutes = require("./posts/postRouter");
 const userRoutes = require("./users/userRouter");
 
-// middleware
+// middleware queue
 server.use(express.json());
 // server.use(helmet());
 // server.use(cors());
+server.use(logger);
 
 // declare routes
 
+server.use(atGate);
 server.use("/posts", postRoutes);
 server.use("/users", userRoutes);
-
-// server.get("/", (req, res) => {
-//   res.send(`<h2>Let's write some middleware!</h2>`);
-// });
+server.use(function(req, res) {
+  res.status(404).send("Ain't nobody got time for that");
+});
 
 //custom middleware
 
-// function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      "Origin"
+    )}`
+  );
+  next();
+}
+
+function atGate(req, res, next) {
+  console.log(`At the gate, about to be eaten`);
+
+  next();
+}
+
+function auth(req, res, next) {
+  if (req.url === "/mellon") {
+    next();
+  } else {
+    res.json("You shall not pass!");
+  }
+}
+
+server.get("/mellon", auth, (req, res) => {
+  console.log("Gate opening...");
+  console.log("Inside and safe");
+  res.send("Welcome weary traveler");
+});
 
 module.exports = server;
